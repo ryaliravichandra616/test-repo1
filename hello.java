10f3a966efadeef2f2d6a97d12d443b84b9eb98f
dev1 <dev1@mail.com>
2025-10-17 12:08:16 +0530

    conflit resolved

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class hello     #from dev2
 {
      public static void main()  #from dev2
      {
-	     system.out.println("hello world")
+	     system.out.println("hello world")  #from dev1
      }
 }   #from dev2
